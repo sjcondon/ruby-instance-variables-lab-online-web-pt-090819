e7c45d3a9ffeeeee547635693fa44bb2a6e5072a
sarah <s.j.condon01@gmail.com>
2019-09-28 17:11:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,10 @@
 class Dog
-  def name (dog_name)
-    lassie = Dog.new
-      lassie.name = 'Lassie'
-   def this_dogs_name = dog_name
+  def name=(dog_name)
+    "Lassie" = dog_name
   end
  
   def name
     this_dogs_name
   end
 end
- 
+"Lassie"
